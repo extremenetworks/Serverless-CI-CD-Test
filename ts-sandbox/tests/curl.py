e0e789bb79845a7d98358b77e62e322039cbe3c7
bitspradp <trust.pradeep@gmail.com>
2019-05-08 16:46:12 +0530

    Update curl.py

--- ts-sandbox/tests/curl.py
+++ ts-sandbox/tests/curl.py
@@ -23,10 +23,10 @@ def outputReader(proc):
     for line in iter(proc.stdout.readline, b''):
         decodedLine = line.decode('utf-8')
         if "RESPONSE" in decodedLine:
-            if '200' or '201' in decodedLine:
+            if decodedLine.find('200') >= 0 or decodedLine.find("201")>=0:
                 createTestOutput(cases[_count], 'SUCCESS', decodedLine, tab)
             else:
-                createTestOutput(cases[_count], 'FAILED', tab)
+                createTestOutput(cases[_count], 'FAILED', decodedLine, tab)
                 isPass = False
             _count += 1
         if _count == num:
