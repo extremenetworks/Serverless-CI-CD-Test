dc75dc9c47a00814186ecdd5a03a19cbe683db87
bitspradp <trust.pradeep@gmail.com>
2019-05-08 16:27:59 +0530

    Update test.js

--- ts-sandbox/test.js
+++ ts-sandbox/test.js
@@ -1,2 +1,4 @@
-napi.get("/user")
-napi.post("/user").form({"userId": "testa", "email": "testaaa@test.com"})
+napi.get("/user/testa")
+napi.post("/user").form({"userId": "testb", "email": "testaaa@test.com"})
+napi.get("/user/testb")
+napi.delete("/user/testb")
